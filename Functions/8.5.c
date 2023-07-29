2311d63dd9c5069bd7ed871539fec27f84ac4815
Jayaweera H R K <135606255+JayaweeraHRK@users.noreply.github.com>
2023-07-29 14:31:13 +0530

    Add files via upload

---
+++ Functions/8.5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sum()
+{
+    int a,b,sum;
+    printf("enter 02 numbers:");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("%d \n",sum);
+}
+int main()
+{
+    sum();
+    sum();
+}
